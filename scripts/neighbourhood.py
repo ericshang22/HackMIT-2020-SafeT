159d894af1488cb600db130ecdfee491df476ed5
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2020-09-19 02:11:32 -0700

    Neighbourhood

--- scripts/neighbourhood.py
+++ scripts/neighbourhood.py
@@ -1,5 +1,39 @@
 import pandas as pd
 
+#due to VSCode limitations, must use absolute path, change in release or when using locally
+neighbourhoods = pd.read_csv('C:/Users/lavao/Documents/GitHub/HackMIT-2020/scripts/Toronto_Neighbourhood.csv')
+#print (neighbourhoods.head())
 
-df = pd.read_excel("CityofToronto_COVID-19_NeighbourhoodData.xlsx", sheet_name="Recent Cases and Rates by Neigh")
-print (df.head())
+neighbourhoods.drop(["Neighbourhood ID"], axis = 1, inplace = True)
+#neighbourhoods.drop(["Missing Address/Postal Code"], axis = 0, inplace = True)
+
+#higher count neighbourhood, sorted by descending
+higher_count = neighbourhoods.sort_values(by=['Case Count'], ascending=False)
+danger_zone = higher_count['Neighbourhood Name'].head(n=6).tolist()
+
+#list comprehension, using lists in place of df
+danger_zone[:] = [x for x in danger_zone if "Missing Address/Postal Code" not in x]
+
+#slimming
+while (len(danger_zone) > 5):
+    del (danger_zone[-1])
+
+#opens absolute path danger.txt in write mode and prints each item of list
+with open("C:/Users/lavao/Documents/GitHub/HackMIT-2020/scripts/danger.txt", "w") as output_file:
+   for item in danger_zone:
+       output_file.write(str(item + '\n'))
+
+#lower count neighbourhood, sorted by ascending
+lower_count = neighbourhoods.sort_values(by=['Case Count'], ascending=True)
+safety_zone = lower_count['Neighbourhood Name'].head(n=6).tolist()
+safety_zone[:] = [x for x in safety_zone if "Missing Address/Postal Code" not in x]
+
+while (len(safety_zone) > 5):
+    del (safety_zone[-1])
+
+with open("C:/Users/lavao/Documents/GitHub/HackMIT-2020/scripts/safety.txt", "w") as output_file:
+   for item in safety_zone:
+       output_file.write(str(item + '\n'))
+
+#print(lower_count)
+#print(higher_count)
