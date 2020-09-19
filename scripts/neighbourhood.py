75dc6c8f8b3bb106e9cce9da3fb097d2f30d24d3
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2020-09-19 01:17:13 -0700

    Convert to csv

--- scripts/neighbourhood.py
+++ scripts/neighbourhood.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 
-df = pd.read_excel(file_name, sheet_name=None)
+df = pd.read_excel("CityofToronto_COVID-19_NeighbourhoodData.xlsx", sheet_name="Recent Cases and Rates by Neigh")
+print (df.head())
